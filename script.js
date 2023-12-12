ae124f9ca7cc13efde7ce811681f2b499e389b4e
MisbahProgrammer <misbahrehman891@gmail.com>
2023-12-12 23:15:56 +0500

    changes color vlaue

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 let rect = document.querySelector(".rect");
-rect.addEventListener('mousemove', function(details){
+rect.addEventListener("mousemove", function (details) {
     let rectlocation = rect.getBoundingClientRect();
     let insideval = details.clientX - rectlocation.left;
-    if(insideval < rectlocation.width/2){
-        let redcolor = gsap.utils.mapRange(0,rectlocation.width / 2,255,0,insideval);
-        gsap.to(rect , {
-            backgroundColor : `rgb(${redcolor}, 0, 0)`,
-            ease : Power4,
+    if (insideval < rectlocation.width / 2) {
+        let redcolor = gsap.utils.mapRange(
+            0,
+            rectlocation.width / 2,
+            255,
+            0,
+            insideval
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redcolor}, 145, 45)`,
+            ease: Power4,
+        });
+    } else {
+        let bluecolor = gsap.utils.mapRange(
+            rectlocation.width / 2,
+            rectlocation.width,
+            0,
+            255,
+            insideval
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(45, 0, ${bluecolor})`,
+            ease: Power4,
         });
     }
-    else{
-        let bluecolor = gsap.utils.mapRange(rectlocation.width/2,rectlocation.width ,0,255,insideval);
-        gsap.to(rect , {
-            backgroundColor : `rgb(0, 0, ${bluecolor})`,
-            ease : Power4,
-        });
-    }
-})
+});
 
-rect.addEventListener("mouseleave", function(){
+rect.addEventListener("mouseleave", function () {
     gsap.to(rect, {
         backgroundColor: "white",
-    })
-})
+    });
+});
